1c2a4253e4ea3a25672ac9941d1b79605c2fae7f
Lorenzo <lorenzo.cap.delacruz@gmail.com>
2024-07-31 19:08:32 +0800

    modify: removed component

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -4,7 +4,17 @@ import LoginPanel from "../components/LoginPanel";
 const Loginpage = () => {
   return (
     <div className="flex flex-col w-screen h-screen bg-slate-100 px-10 py-2">
-      <Header />
+      <div className="flex flex-row justify-between h-20 w-full">
+        <div className="flex flex-row items-center space-x-3">
+          <img src="/logo.png" alt="" className="w-14 h-14" />
+          <span className="outfit text-4xl text-black">Medicare</span>
+        </div>
+        <div className="flex flex-row items-center justify-end h-full px-20 space-x-24">
+          <button className="outfit text-black">About</button>
+          <button className="outfit text-black">Services</button>
+          <button className="outfit text-black">Contact</button>
+        </div>
+      </div>
       <div className="flex items-center justify-center w-full h-full">
         <div className="flex flex-col w-[35%] space-y-5">
           <span className="outfit-bold text-black text-5xl text-center">
